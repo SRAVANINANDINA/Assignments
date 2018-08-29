09d67c3134f42ebbab64b1411db0503cf82b158e
Sravani Sai Nandina <sravanisainandina@Sravanis-MacBook-Air.local>
2018-08-29 11:56:52 -0700

    TestAnswer

---
+++ StringLength.java
@@ -0,0 +1,22 @@
+public class StringLength {
+
+    public static void main(String[] args) {
+
+
+        int a=0;
+
+
+       String name = "this is a string";
+
+        char[] stringToCharArray = name.toCharArray();
+
+        for (char output : stringToCharArray) {
+            a++;
+
+
+
+
+         }
+        System.out.println(a);
+    }
+}
